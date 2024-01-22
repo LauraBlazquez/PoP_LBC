6123515b3f3d2db60fbf6062a8667cba5b825fd9
LauraBlazquez <laura.blazquez.7e7@itb.cat>
2024-01-22 11:57:13 +0100

    refactoring task closes #1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine ("Hello, World!");
+﻿namespace PoPRefactoring;
+
+public class PopProject
+{
+    public static void Main ()
+    {
+        int dia, mes, any;
+        Console.WriteLine ("Introdueix el dia, mes i any");
+        dia = Convert.ToInt32 (Console.ReadLine ());
+        mes = Convert.ToInt32 (Console.ReadLine ());
+        any = Convert.ToInt32 (Console.ReadLine ());
+        Console.WriteLine(Valida(dia, mes, any) ? "El format no és correcte" : "La data és correcta.");
+    }
+
+    public static bool Valida (int day, int month, int year)
+    {
+        int totalDaysMonth;
+        switch (month)
+        {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12: totalDaysMonth = 31; 
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11: totalDaysMonth = 30; break;
+            case 2: // verifica l'any de traspàs
+                if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) totalDaysMonth = 29;
+                else totalDaysMonth = 28;
+                break;
+            default: return false;
+        }
+        if (day > totalDaysMonth) return false;
+        else return true;
+    }
+}
